fe499495432956fc8e3d76d728f6a3d9cb6d3b29
DevPOCapital <developers@pocapital.com.au>
2026-06-09 16:46:33 +1000

    v0806z6

--- app_streamlit.py
+++ app_streamlit.py
@@ -7815,8 +7815,10 @@ Set-Content "C:\\Users\\willp\\RateEdge Swaption Pricer\\.env" "RATEEDGE_DB_URL=
 
         with col4:
             st.markdown("**Platform & Action**")
-            _BROKER_MICS = ["BGCD", "BILT", "DWSF", "GSEF", "IGDL", "ISWE", "ISWV",
-                           "TPSE", "TSEF", "TWSF", "XXXX"]
+            _BROKER_MICS = ["BGCD", "BGCO", "BGCI", "AURO", "BILT", "DWSF", "GSEF", "GFSO",
+                           "IGDL", "ISWE", "ISWV", "IOIR", "IMRD",
+                           "TPSE", "TPIR", "TPEU", "TSEF", "TSIR", "TSAF", "TWSF", "TWEM",
+                           "BBSF", "BMTF", "BTFE", "HSBC", "XOFF", "XXXX"]
             _all_platforms = sorted(_BROKER_MICS, key=lambda p: PLATFORM_NAMES.get(p, p))
             _platform_display = [f"{PLATFORM_NAMES.get(p, p)} ({p})" for p in _all_platforms]
             _platform_map = {f"{PLATFORM_NAMES.get(p, p)} ({p})": p for p in _all_platforms}
